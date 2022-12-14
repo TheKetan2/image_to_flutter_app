6963ca70cf4b89fc0e47dec22da307d1fcac713a
Ketan Ramteke <Ketantoo@gmail.com>
2022-12-14 22:02:01 +0530

    added share and copy buttons

--- image_to_flutter_app/lib/recognition_screen.dart
+++ image_to_flutter_app/lib/recognition_screen.dart
@@ -12,6 +12,31 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: () {},
+            child: Icon(
+              Icons.copy,
+              size: 28,
+            ),
+          ),
+          SizedBox(
+            width: 8,
+          ),
+          FloatingActionButton(
+            backgroundColor: Color(0xffec360e),
+            heroTag: null,
+            onPressed: () {},
+            child: Icon(
+              Icons.share,
+              size: 28,
+            ),
+          ),
+        ],
+      ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Container(
@@ -19,10 +44,30 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
           child: Column(
             children: [
               SizedBox(
-                height: 55 + MediaQuery.of(context).viewInsets.top,
+                height: 60 + MediaQuery.of(context).viewInsets.top,
               ),
               Text(
                 "Text Recognition",
+                style: textStyle(
+                  30,
+                  Color.fromARGB(255, 105, 186, 253),
+                  FontWeight.w800,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Image(
+                width: 256,
+                height: 256,
+                image: AssetImage('images/add_file.png'),
+                fit: BoxFit.fill,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Lorem Ipsum tipsum",
                 style: textStyle(
                   30,
                   Color.fromARGB(255, 105, 186, 253),

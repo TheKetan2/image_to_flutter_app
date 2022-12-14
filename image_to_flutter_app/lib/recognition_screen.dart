126916cd65d5e9165d734768a9af0643cebe3614
Ketan Ramteke <Ketantoo@gmail.com>
2022-12-14 23:01:03 +0530

    can add image from gallery as well as camera now

--- image_to_flutter_app/lib/recognition_screen.dart
+++ image_to_flutter_app/lib/recognition_screen.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:image_to_flutter_app/utils.dart';
+import 'dart:io';
 
 class RecognitionScreen extends StatefulWidget {
   const RecognitionScreen({Key? key}) : super(key: key);
@@ -9,6 +13,60 @@ class RecognitionScreen extends StatefulWidget {
 }
 
 class _RecognitionScreenState extends State<RecognitionScreen> {
+  dynamic pickedImage = null;
+  optionsDialogue(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            children: [
+              SimpleDialogOption(
+                onPressed: () => pickImage(ImageSource.gallery),
+                child: Text(
+                  "Gallary",
+                  style: textStyle(
+                    20,
+                    Colors.black,
+                    FontWeight.w400,
+                  ),
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () => pickImage(ImageSource.camera),
+                child: Text(
+                  "Camera",
+                  style: textStyle(
+                    20,
+                    Colors.black,
+                    FontWeight.w400,
+                  ),
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () => Navigator.pop(context),
+                child: Text(
+                  "Cancel",
+                  style: textStyle(
+                    20,
+                    Colors.black,
+                    FontWeight.w400,
+                  ),
+                ),
+              )
+            ],
+          );
+        });
+  }
+
+  pickImage(ImageSource imageSource) async {
+    final image = await ImagePicker().getImage(source: imageSource);
+    print("path: " + File(image.path).toString());
+    setState(() {
+      pickedImage = File(image.path);
+    });
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,11 +115,21 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
               SizedBox(
                 height: 30,
               ),
-              Image(
-                width: 256,
-                height: 256,
-                image: AssetImage('images/add_file.png'),
-                fit: BoxFit.fill,
+              InkWell(
+                onTap: () => optionsDialogue(context),
+                child: pickedImage == null
+                    ? Image(
+                        width: 256,
+                        height: 256,
+                        image: AssetImage('images/add_file.png'),
+                        fit: BoxFit.fill,
+                      )
+                    : Image(
+                        image: FileImage(pickedImage),
+                        width: 256,
+                        height: 256,
+                        fit: BoxFit.fill,
+                      ),
               ),
               SizedBox(
                 height: 30,
